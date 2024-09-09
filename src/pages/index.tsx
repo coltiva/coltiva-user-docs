1446ea41e71f950d91582045b94f5ddf333f3507
Felix Eriksson <felix.eriksson@revibeenergy.com>
2024-09-09 12:07:11 +0200

    Dummy change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Getting Started with Coltiva
           </Link>
         </div>
       </div>
